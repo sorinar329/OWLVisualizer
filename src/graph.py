a19ae91c5387a3adc9b98ad58763105f0e8b75fb
sl1ces <65294564+sl1ces@users.noreply.github.com>
2024-02-19 15:08:34 +0100

    process graph to visualize class restrictions

--- src/graph.py
+++ src/graph.py
@@ -3,55 +3,88 @@ from rdflib import Graph, OWL, RDFS, RDF
 from rdflib.term import BNode
 
 knowledge_graph = Graph()
-knowledge_graph.parse("/home/mkuempel/Downloads/mixing.owl")
+knowledge_graph.parse("data/mixing.owl")
 
+graph_to_visualize = {'nodes': [], 'edges': []}
 
-# for o, s in knowledge_graph.subject_objects(RDFS.subClassOf):
-#   print(type(s))
-# for _, _, intersection in knowledge_graph.triples((s, OWL.intersectionOf, None)):
-#     for _,_,first in knowledge_graph.triples((intersection, RDF.first, None)):
-#         for property, value in knowledge_graph.predicate_objects(first):
-#             print(property, value)
-#
-#     for _,_,rest in knowledge_graph.triples((intersection, RDF.rest, None)):
-#         for _,_,first in knowledge_graph.triples((rest, RDF.first, None)):
-#             for property, value in knowledge_graph.predicate_objects(first):
-#                 print(property, value)
-# for _,_, property in knowledge_graph.triples((first, OWL.onProperty, None)):
-#      print(property)
-# for _,_, value in knowledge_graph.triples((first, OWL.hasValue, None)):
-#      print(value)
+restriction_type = [OWL.hasValue, OWL.someValuesFrom, OWL.allValuesFrom]
+collection_type = {OWL.intersectionOf: 'Intersection', OWL.unionOf: 'Union'}
 
 
 def get_all_classes(kg: Graph):
-    for superclass, subclass in kg.subject_objects(RDFS.subClassOf):
-        if isinstance(subclass, BNode):
+    for subclass, superclass in kg.subject_objects(RDFS.subClassOf):
+        if isinstance(superclass, BNode):
             continue
-        print(superclass, subclass)
+        graph_to_visualize.get("edges").append({'from': subclass, 'to': superclass})
+        graph_to_visualize.get("nodes").append({'node_id': superclass})
+        graph_to_visualize.get("nodes").append({'node_id': subclass})
 
 
-def get_collection_from_class():
-    for _, _, intersection in knowledge_graph.triples((None, OWL.intersectionOf, None)):
-        for list_triple in knowledge_graph.triples((intersection, None, None)):
-            extract_property_value_from_list(list_triple)
-        break
-
-
-def extract_property_value_from_list(triple):
+def extract_collection_members(triple, parent_node):
     _, p, el = triple
     if p == RDF.first:
         edge, node = None, None
         for _, p2, el2 in knowledge_graph.triples((el, None, None)):
             if p2 == OWL.onProperty:
                 edge = el2
-            if p2 == OWL.hasValue:
+            if p2 in restriction_type:
                 node = el2
-        print(edge, node)
+        if isinstance(node, BNode) and is_restriction(node):
+            return
+            # extract_nested_restrictions(node)
+        graph_to_visualize.get("nodes").append(node)
+        graph_to_visualize.get("edges").append({'from': node, 'to': parent_node, 'label': edge})
 
     else:
         for list_rest_triple in knowledge_graph.triples((el, None, None)):
-            extract_property_value_from_list(list_rest_triple)
+            extract_collection_members(list_rest_triple, parent_node)
 
 
-# get_all_classes(knowledge_graph)
-get_collection_from_class()
+def extract_nested_restrictions(bnode: BNode):
+    for _, p, el in knowledge_graph.triples((bnode, None, None)):
+        if p == OWL.onProperty:
+            print(el)
+
+        if p in restriction_type:
+            print(el)
+            if isinstance(el, BNode) and is_restriction(el):
+                extract_nested_restrictions(el)
+
+
+def is_list(bnode: BNode):
+    for _, _, c in knowledge_graph.triples((bnode, None, None)):
+        for p in knowledge_graph.predicates(c):
+            if p == RDF.first:
+                return True
+    return False
+
+
+def is_restriction(bnode: BNode):
+    matched_triple = list(knowledge_graph.triples((bnode, RDF.type, OWL.Restriction)))
+    return len(matched_triple) > 0
+
+
+def get_class_restrictions(knowledge_graph):
+    classes_with_restrictions = [(_, res) for (_, res)
+                                 in knowledge_graph.subject_objects(RDFS.subClassOf)
+                                 if isinstance(res, BNode)]
+    for x, y in classes_with_restrictions:
+        for bnode, p, bnode_or_value in knowledge_graph.triples((y, None, None)):
+            if is_restriction(bnode=bnode):
+                continue
+                # extract_nested_restrictions(bnode=bnode)
+            else:
+                if collection_type.get(p) is not None:
+                    extract_collection_members((None, p, bnode_or_value), "Intersection")
+
+
+def equivalent_properties(kg):
+    for cls, p, cls_or_bnode in kg.triples((None, OWL.equivalentClass, None)):
+        if is_restriction(bnode=cls_or_bnode):
+            extract_nested_restrictions(cls_or_bnode)
+
+
+def get_graph_to_visualize():
+    get_all_classes(knowledge_graph)
+    get_class_restrictions(knowledge_graph)
+    return graph_to_visualize
