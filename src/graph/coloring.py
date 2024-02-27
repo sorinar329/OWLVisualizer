0e0280399b4cf0e2e2de7a5c352ec43a636957dd
sl1ces <65294564+sl1ces@users.noreply.github.com>
2024-02-27 12:36:59 +0100

    formatting and remove print statements

--- src/graph/coloring.py
+++ src/graph/coloring.py
@@ -1,6 +1,4 @@
-from src.graph.graph_utility import uri_or_literal_2label
-
-
+from rdflib import OWL
 def color_classes(graph: dict):
     nodes = [n for n in graph.get("nodes") if 'Task' in n.get("id")]
     for task in nodes:
@@ -9,7 +7,6 @@ def color_classes(graph: dict):
     nodes = [n for n in graph.get("nodes") if 'Motion' in n.get("id")]
     for motion in nodes:
         motion.update({'color': {"background": "orangered", "border": "black"}})
-
     nodes = [n for n in graph.get("nodes") if 'Motion' not in n.get("id") and 'Task' not in n.get("id")]
     for node in nodes:
         node.update({"color": "lightblue"})
@@ -19,9 +16,6 @@ def color_parameters(graph: dict):
     for edge in graph.get('edges'):
         relation = edge.get('label')
         if relation != 'subClassOf' and relation != 'owl:intersectionOf':
-
             child = edge.get("from")
-            print(child)
-            print(edge.get('to'))
             node = [n for n in graph.get("nodes") if n.get("id") == child][0]
             node.update({'color': {"background": "yellow", "border": "black"}})
