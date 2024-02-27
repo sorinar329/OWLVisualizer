93990393b645534fb8f5746ed03541a6d6e42b21
sl1ces <65294564+sl1ces@users.noreply.github.com>
2024-02-27 12:37:27 +0100

    use mocked solution for query builder

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, jsonify
-import src.graph.graph
-import src.graph.coloring
-from src.graph.graph_utility import uri_or_literal_2label
+from flask import Flask, render_template, jsonify, request
+import graph.graph
+import graph.coloring
+import query_builder
 
 app = Flask(__name__)
 
@@ -13,18 +13,27 @@ def index():
 
 @app.route('/get_graph_data_rdf')
 def get_graph_data_rdf():
-    graph_visualize = src.graph.graph.get_graph_to_visualize()
-    src.graph.coloring.color_classes(graph_visualize)
-    src.graph.coloring.color_parameters(graph_visualize)
+    graph_visualize = graph.graph.get_graph_to_visualize()
+    graph.coloring.color_classes(graph_visualize)
+    graph.coloring.color_parameters(graph_visualize)
     return jsonify({'nodes': graph_visualize.get("nodes"), 'edges': graph_visualize.get("edges")})
 
 
 @app.route('/suggest_classes', methods=["GET"])
 def suggest_classes():
-    graph_visualize = src.graph.graph.get_graph_to_visualize()
+    graph_visualize = graph.graph.get_graph_to_visualize()
     classes = [i.get('id') for i in graph_visualize.get("nodes")]
     print(classes)
     return jsonify(classes)
+
+
+@app.route('/query_builder', methods=["GET"])
+def suggest_triples():
+    qb = query_builder.get_query_builder()
+    # if request.method == "POST":
+    #     print("Hi")
+    # else:
+    return jsonify(qb.mock_suggestion())
 
 
 if __name__ == '__main__':
---
+++ src/query_builder.py
@@ -0,0 +1,44 @@
+from rdflib import Graph, URIRef, RDF, OWL
+import rdflib.util
+
+motion = "http://www.ease-crc.org/ont/mixing#WhirlstormMotion"
+knowledge_graph = Graph()
+knowledge_graph.parse("data/mixing.owl")
+
+
+class QueryBuilder:
+    def __init__(self, knowledge_graph: rdflib.Graph):
+        self.kg = knowledge_graph
+        self.triples = []
+        self.sparql_query = "SELECT * WHERE { "
+
+    def add_triple(self, triple: ()):
+        self.triples.append(triple)
+
+    def suggest_triples(self, triple: ()):
+        if triple[0] is None:
+            return self.kg.triples(triple)
+        elif triple[0] is not None:
+            return self.kg.predicate_objects(triple[0])
+
+        for s, p, o in self.kg.triples(triple):
+            print(s, p, o)
+
+    def mock_suggestion(self):
+        suggestions = list(self.kg.triples((URIRef(motion), None, None)))
+        mocked_solution = []
+        for s, p, o in suggestions:
+            mocked_solution.append([str(s), str(p), str(o)])
+        return mocked_solution
+        # for s, p, o in self.kg.triples((URIRef(motion), None, None)):
+        #     print(s, p, o)
+        # for s2, p2, o2 in self.kg.triples((o, OWL.intersectionOf, None)):
+        #     print(p2, o2)
+        #     for s3, p3, o3 in self.kg.triples((o2, RDF.first, None)):
+        #         print(s3, p3, o3)
+        #         for s4, p4, o4 in self.kg.triples((o3, None, None)):
+        #             print(s4, p4, o4)
+
+
+def get_query_builder():
+    return QueryBuilder(knowledge_graph)
