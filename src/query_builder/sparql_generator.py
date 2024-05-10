fc151ed8832a6bb4f2db328a2e3540899abf491a
sl1ces <65294564+sl1ces@users.noreply.github.com>
2024-05-10 16:19:21 +0200

    started SparqlGenerator class

---
+++ src/query_builder/sparql_generator.py
@@ -0,0 +1,7 @@
+# import rdflib
+# class SparqlGenerator:
+#     def __init__(self, knowledge_graph: rdflib.Graph, triples: list[tuple]):
+#         self.knowledge_graph = knowledge_graph
+#         self.triples = triples
+#
+#     def generate_sparql_query(self):
