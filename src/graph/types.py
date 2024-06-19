e60f0a82adfe07c5a232de8b7cc2845e6f2774ad
sl1ces <65294564+sl1ces@users.noreply.github.com>
2024-06-19 14:33:31 +0200

    add inverse dicts to get properties from string.

--- src/graph/types.py
+++ src/graph/types.py
@@ -12,21 +12,33 @@ def get_cardinality_name(p: URIRef):
     return cardinality_types.get(p)
 
 
+def get_cardinality_type(p: str):
+    return {v: k for (k, v) in cardinality_types.items()}.get(p)
+
+
 def get_collection_name(p: URIRef):
     return collection_type.get(p)
+
+
+def get_collection_type(p: str):
+    return {v: k for (k, v) in collection_type.items()}.get(p)
 
 
 def get_restriction_name(p: URIRef):
     return restriction_type.get(p)
 
 
+def get_restriction_type(p: str):
+    return {v: k for (k, v) in restriction_type.items()}.get(p)
+
+
 def is_collection(p: URIRef):
-    return p in collection_type
+    return p in collection_type or p in {v: k for (k, v) in collection_type.items()}
 
 
 def is_restriction(p: URIRef):
-    return p in restriction_type
+    return p in restriction_type or p in {v: k for (k, v) in restriction_type.items()}
 
 
 def is_cardinality(p: URIRef):
-    return p in cardinality_types
+    return p in cardinality_types or p in {v: k for (k, v) in cardinality_types.items()}
