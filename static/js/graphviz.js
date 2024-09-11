0d1f0b8a9801a5440f9e3ceea918734e1855897d
sl1ces <65294564+sl1ces@users.noreply.github.com>
2024-09-11 07:35:04 +0200

    add error alert

--- static/js/graphviz.js
+++ static/js/graphviz.js
@@ -10,39 +10,42 @@ function vizGraph() {
     fetch('/get_graph_data_rdf')
         .then(response => response.json())
         .then(data => {
-            const container = document.getElementById('vis-network');
-            const options = {
-                physics: {
-                    adaptiveTimestep: true,
-                    barnesHut: {
-                        gravitationalConstant: -7000,
-                        springConstant: 0.04,
-                        springLength: 100,
+            if (data.error) {
+                createAlert(data.error, "danger");
+            } else {
+                const container = document.getElementById('vis-network');
+                const options = {
+                    physics: {
+                        adaptiveTimestep: true,
+                        barnesHut: {
+                            gravitationalConstant: -7000,
+                            springConstant: 0.04,
+                            springLength: 100,
+                        },
+                        timestep: 0.3
                     },
-                    timestep: 0.3
-                },
-                layout: {
-                    improvedLayout: false
-                },
-            };
-            const nodesDataset = new vis.DataSet(data.nodes)
-            const edgesDataset = new vis.DataSet(data.edges)
-            const network = new vis.Network(container, {nodes: nodesDataset, edges: edgesDataset}, options);
-            const searchBar = document.getElementById("searchBar");
-            data = network.body.data.nodes._data;
-            autocompleteInput(data);
-            const searchButton = document.getElementById("searchButton");
-            searchButton.addEventListener("click", function () {
-                viewNode(network, data);
-            });
-            searchBar.addEventListener("keypress", function (event) {
-                if (event.key === "Enter") {
+                    layout: {
+                        improvedLayout: false
+                    },
+                };
+                const nodesDataset = new vis.DataSet(data.nodes)
+                const edgesDataset = new vis.DataSet(data.edges)
+                const network = new vis.Network(container, {nodes: nodesDataset, edges: edgesDataset}, options);
+                const searchBar = document.getElementById("searchBar");
+                data = network.body.data.nodes._data;
+                autocompleteInput(data);
+                const searchButton = document.getElementById("searchButton");
+                searchButton.addEventListener("click", function () {
                     viewNode(network, data);
-                }
-            })
-
+                });
+                searchBar.addEventListener("keypress", function (event) {
+                    if (event.key === "Enter") {
+                        viewNode(network, data);
+                    }
+                })
+            }
 
         })
-        .catch(error => console.error('Error fetching data:', error));
+        .catch(error => createAlert(error, "danger"));
 }
 
